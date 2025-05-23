f90cc2fb4b02423c1e5d21c58a3361a4166af6b6
frndsluan <fernaluan4@gmail.com>
2025-05-23 18:26:49 +0000

    Aula 23/05/2025 - Salvando por que nao salvei no dia anterior

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,30 @@
 import "./styles/theme.css";
 import "./styles/global.css";
+import { MyText } from "./components/MyText";
 
-function App() {
+export default function App() {
+  //React Fragment
   return (
-    <div>
-      <h1>Meu primeiro React App</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem minus
-        excepturi eius, rem ea veniam accusamus. Sunt consequatur, dolorum quasi
-        officia voluptatibus unde quia facilis reprehenderit numquam quo, sed
-        aut!
-      </p>
-    </div>
+    <>
+      <MyText title="Meu titulo 1">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. At similique
+        voluptatem minima nam explicabo. Modi suscipit ut obcaecati consequuntur
+        porro molestias, temporibus vero iste perspiciatis aperiam ipsam, soluta
+        et error.
+      </MyText>
+      <MyText title="Meu título 2">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Harum soluta
+        cupiditate, nobis excepturi id nihil asperiores exercitationem molestiae
+        quia accusamus praesentium debitis voluptate corporis, officiis,
+        nesciunt culpa iste dignissimos odio!
+      </MyText>
+      <MyText title="Meu título 3">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+        voluptatibus, cumque, doloribus, voluptates quisquam voluptatibus
+        voluptatibus, cumque, doloribus, voluptates quisquam voluptatibus,
+        cumque, doloribus, voluptates quisquam voluptatibus, cumque, doloribus
+      </MyText>
+    </>
   );
 }
-
-export default App;
+// export default function App() {
