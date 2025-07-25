5d13a7fb3115f16c184b82ffccda7a6f0a116968
frndsluan <fernaluan4@gmail.com>
2025-07-25 23:59:51 +0000

    Desafio de programação 7 - Definitive Edition (Luan Fernandes)

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,7 +1,6 @@
 import styles from "./Product.module.css";
 import { useContext } from "react";
 import { CartContext } from "../service/CartContext";
-import { Link } from "react-router";
 
 export function Product({ product }) {
   const { addToCart } = useContext(CartContext);
