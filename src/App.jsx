588a3b0484f9f19ef653248613b5d68e4c512d83
frndsluan <fernaluan4@gmail.com>
2025-09-29 14:00:19 +0000

    Aula 24/09 - Telas de Sign In e Register

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,23 @@ import "./styles/theme.css";
 import "./styles/global.css";
 import { ProductList } from "./components/ProductList";
 import { Header } from "./components/MyHeader";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router";
 import { Cart } from "./components/Cart";
 import { CartProvider } from "./context/CartContext";
+import { Login } from "./components/Login";
+import { ToastContainer } from "react-toastify";
 
 export default function App() {
   return (
     <>
+      <ToastContainer />
       <CartProvider>
         <Header />
         <Routes>
           <Route path="/" element={<ProductList />} />
           <Route path="/cart" element={<Cart />} />
+          <Route path="/login" element={<Login value="login" />} />
+          <Route path="/register" element={<Login value="register" />} />
         </Routes>
       </CartProvider>
     </>
